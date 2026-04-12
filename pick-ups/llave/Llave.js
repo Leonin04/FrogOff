9dd74429ec77818ad07de71bc02240e15ea8c1e9
Leonin04 <davidbacasposadas@gmail.com>
2026-04-12 17:44:04 +0200

    Creada llave, falta cuerda

---
+++ pick-ups/llave/Llave.js
@@ -0,0 +1,95 @@
+import * as THREE from 'three'
+import * as CSG from '../../libs/three-bvh-csg.js'
+
+class Llave extends THREE.Object3D {
+
+  constructor(gui, titleGui) {
+    super();
+
+    this.tama = 0.1;
+    this.altura = this.tama/2
+
+    this.matLlave = new THREE.MeshStandardMaterial({ color: 0xF2CB3F });
+
+    this.createGUI(gui, titleGui);
+
+    this.llave = this.crearLlave();
+
+    this.add(this.llave);
+  }
+
+  createGUI(gui, titleGui) {
+    // Controles para el movimiento de la parte móvil
+    this.guiControls = {
+      rotacion: 0
+    }
+
+    // Se crea una sección para los controles de la caja
+    var folder = gui.addFolder(titleGui);
+    // Estas lineas son las que añaden los componentes de la interfaz
+    // Las tres cifras indican un valor mínimo, un máximo y el incremento
+    folder.add(this.guiControls, 'rotacion', -0.125, 0.2, 0.001)
+      .name('Apertura : ')
+      .onChange((value) => this.setAngulo(-value));
+  }
+
+  crearLlave() {
+    var llave = new THREE.Object3D();
+    
+    var redondez = new THREE.CylinderGeometry(this.tama, this.tama, this.altura);
+
+    var hueco = new THREE.CylinderGeometry(this.tama*0.7, this.tama*0.7, this.altura*2);
+
+
+    var redondezBrush = new CSG.Brush(redondez, this.matLlave);
+    var huecoBrush = new CSG.Brush(hueco, this.matLlave);
+
+    var evaluador = new CSG.Evaluator();
+    var resultado = evaluador.evaluate(redondezBrush, huecoBrush, CSG.SUBTRACTION);
+
+    
+    llave.add(resultado);
+
+    var alargado = new THREE.Mesh(
+      new THREE.CylinderGeometry(this.altura/2, this.altura/2, this.tama * 2.5), this.matLlave
+    );
+
+    alargado.rotateZ(Math.PI / 2);
+
+    alargado.position.set(this.tama*2.2, 0, 0);
+
+    llave.add(alargado);
+
+    var pata1 = new THREE.Mesh(
+        new THREE.CylinderGeometry(this.tama/4, this.tama/4, this.tama), this.matLlave
+    );
+
+    pata1.position.set(this.tama*2.4, 0, this.tama/2);
+
+    pata1.rotateX(Math.PI / 2);
+
+    llave.add(pata1);
+
+    var pata2 = pata1.clone();
+    
+    pata2.position.set(this.tama*3.1, 0, this.tama/2);
+
+    llave.add(pata2);
+
+    return llave;
+  }
+
+  crearCuerda(){
+    
+  }
+
+
+  setAngulo(valor) {
+    this.cuerpo.rotation.z = valor;
+  }
+
+  update() {
+  }
+}
+
+export { Llave }
